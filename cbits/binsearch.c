10eaf2e158aa6f1e621c1ae9414dd7e02bcaa04b
Mark Wotton <mwotton@gmail.com>
2017-01-09 21:49:20 -0500

    formatting

--- cbits/binsearch.c
+++ cbits/binsearch.c
@@ -9,14 +9,11 @@ comparator (const void *key, const void *member)
   unsigned int keyU = (*((unsigned int *) key));
   unsigned int memberU = (*((unsigned int *) member));
 
-  if (keyU < memberU)
-    {
+  if (keyU < memberU) {
       return -1;
-    }
-  else if (keyU > memberU)
-    {
+  } else if (keyU > memberU) {
       return 1;
-    }
+  }
   return 0;
 }
 
@@ -26,13 +23,10 @@ findString (unsigned int index)
   unsigned int *out_index =
     bsearch (&index, &keylookup, 8922, sizeof (unsigned int), &comparator);
 
-  if (out_index == NULL)
-    {
-      // not found
-      return "";
-    }
-  else
-    {
-      return toASCIILut[out_index - keylookup];
-    }
+  if (out_index == NULL) {
+    // not found
+    return "";
+  } else {
+    return toASCIILut[out_index - keylookup];
+  }
 }
