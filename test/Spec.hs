2b9c538bb2306ec25b48cc6afdf9708c4fcc23f2
Mark Wotton <mwotton@gmail.com>
2017-01-10 12:01:10 -0500

    add some tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,4 +1,7 @@
+import           Debug.Trace
 import           Test.Hspec
+import           Test.Hspec.QuickCheck
+import           Test.QuickCheck
 import           Text.Unidecode
 
 main :: IO ()
@@ -7,3 +10,6 @@ main = hspec spec
 spec = describe "unidecode" $ do
   it "doesn't hurt ascii text" $ do
     unidecode 'a' `shouldBe` "a"
+
+  it "doesn't crash" $ property $
+    \x ->  unidecode x == unidecode x
